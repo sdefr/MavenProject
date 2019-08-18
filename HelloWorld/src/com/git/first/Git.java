432bf4a010a5df724d59bd16e6c3dc0b956d7587
sdefr <nktb18@gmail.com>
2019-08-18 11:22:06 -0700

    Second commit

--- HelloWorld/src/com/git/first/Git.java
+++ HelloWorld/src/com/git/first/Git.java
@@ -3,7 +3,7 @@ package com.git.first;
 public class Git {
 	
 		  public static void main(String[] args) {
-		    System.out.println("Git is fun");
+		    System.out.println("Git is fun and cool");
 
 		  }
 }
